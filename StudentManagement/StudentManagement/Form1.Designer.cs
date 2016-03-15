fe17810ec295f1743d91f543fcb7a3612f40415a
Hung Q. Nguyen <hnguyenworkstation@gmail.com>
2016-03-15 15:12:08 -0400

    Finished editing basic Form for Student Management Application

---
+++ StudentManagement/StudentManagement/Form1.Designer.cs
@@ -0,0 +1,354 @@
+﻿namespace StudentManagement
+{
+    partial class frmStudentManage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.gbStudentInfo = new System.Windows.Forms.GroupBox();
+            this.lblStudentID = new System.Windows.Forms.Label();
+            this.lblStudentName = new System.Windows.Forms.Label();
+            this.lblStudentDOB = new System.Windows.Forms.Label();
+            this.lblStudentGender = new System.Windows.Forms.Label();
+            this.lblClass = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.txtStudentID = new System.Windows.Forms.TextBox();
+            this.txtStudentName = new System.Windows.Forms.TextBox();
+            this.dtpDayOfBirth = new System.Windows.Forms.DateTimePicker();
+            this.rdbMale = new System.Windows.Forms.RadioButton();
+            this.rdbFemale = new System.Windows.Forms.RadioButton();
+            this.cbClass = new System.Windows.Forms.ComboBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.dgvStudentInfomation = new System.Windows.Forms.DataGridView();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.gbStudentInfo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudentInfomation)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Coral;
+            this.label1.Location = new System.Drawing.Point(201, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(515, 57);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "STUDENT MANAGEMENT";
+            // 
+            // gbStudentInfo
+            // 
+            this.gbStudentInfo.Controls.Add(this.txtAddress);
+            this.gbStudentInfo.Controls.Add(this.cbClass);
+            this.gbStudentInfo.Controls.Add(this.rdbFemale);
+            this.gbStudentInfo.Controls.Add(this.rdbMale);
+            this.gbStudentInfo.Controls.Add(this.dtpDayOfBirth);
+            this.gbStudentInfo.Controls.Add(this.txtStudentName);
+            this.gbStudentInfo.Controls.Add(this.txtStudentID);
+            this.gbStudentInfo.Controls.Add(this.lblAddress);
+            this.gbStudentInfo.Controls.Add(this.lblClass);
+            this.gbStudentInfo.Controls.Add(this.lblStudentGender);
+            this.gbStudentInfo.Controls.Add(this.lblStudentDOB);
+            this.gbStudentInfo.Controls.Add(this.lblStudentName);
+            this.gbStudentInfo.Controls.Add(this.lblStudentID);
+            this.gbStudentInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbStudentInfo.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.gbStudentInfo.Location = new System.Drawing.Point(43, 114);
+            this.gbStudentInfo.Name = "gbStudentInfo";
+            this.gbStudentInfo.Size = new System.Drawing.Size(840, 299);
+            this.gbStudentInfo.TabIndex = 1;
+            this.gbStudentInfo.TabStop = false;
+            this.gbStudentInfo.Text = "Student Infomation";
+            this.gbStudentInfo.Enter += new System.EventHandler(this.gbStudentInfo_Enter);
+            // 
+            // lblStudentID
+            // 
+            this.lblStudentID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStudentID.Location = new System.Drawing.Point(75, 56);
+            this.lblStudentID.Name = "lblStudentID";
+            this.lblStudentID.Size = new System.Drawing.Size(114, 26);
+            this.lblStudentID.TabIndex = 0;
+            this.lblStudentID.Text = "Student ID:";
+            this.lblStudentID.Click += new System.EventHandler(this.lblStudentID_Click);
+            // 
+            // lblStudentName
+            // 
+            this.lblStudentName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStudentName.Location = new System.Drawing.Point(44, 134);
+            this.lblStudentName.Name = "lblStudentName";
+            this.lblStudentName.Size = new System.Drawing.Size(136, 26);
+            this.lblStudentName.TabIndex = 1;
+            this.lblStudentName.Text = "Student Name:";
+            this.lblStudentName.Click += new System.EventHandler(this.lblStudentName_Click);
+            // 
+            // lblStudentDOB
+            // 
+            this.lblStudentDOB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStudentDOB.Location = new System.Drawing.Point(44, 221);
+            this.lblStudentDOB.Name = "lblStudentDOB";
+            this.lblStudentDOB.Size = new System.Drawing.Size(136, 26);
+            this.lblStudentDOB.TabIndex = 2;
+            this.lblStudentDOB.Text = "Student DOB:";
+            this.lblStudentDOB.Click += new System.EventHandler(this.lblStudentDOB_Click);
+            // 
+            // lblStudentGender
+            // 
+            this.lblStudentGender.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStudentGender.Location = new System.Drawing.Point(447, 56);
+            this.lblStudentGender.Name = "lblStudentGender";
+            this.lblStudentGender.Size = new System.Drawing.Size(75, 26);
+            this.lblStudentGender.TabIndex = 3;
+            this.lblStudentGender.Text = "Gender:";
+            this.lblStudentGender.Click += new System.EventHandler(this.lblStudentGender_Click);
+            // 
+            // lblClass
+            // 
+            this.lblClass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClass.Location = new System.Drawing.Point(458, 131);
+            this.lblClass.Name = "lblClass";
+            this.lblClass.Size = new System.Drawing.Size(64, 26);
+            this.lblClass.TabIndex = 4;
+            this.lblClass.Text = "Class:";
+            this.lblClass.Click += new System.EventHandler(this.lblClass_Click);
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAddress.Location = new System.Drawing.Point(439, 221);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(83, 26);
+            this.lblAddress.TabIndex = 5;
+            this.lblAddress.Text = "Address:";
+            this.lblAddress.Click += new System.EventHandler(this.lblAddress_Click);
+            // 
+            // txtStudentID
+            // 
+            this.txtStudentID.Location = new System.Drawing.Point(186, 56);
+            this.txtStudentID.Name = "txtStudentID";
+            this.txtStudentID.Size = new System.Drawing.Size(200, 26);
+            this.txtStudentID.TabIndex = 6;
+            // 
+            // txtStudentName
+            // 
+            this.txtStudentName.Location = new System.Drawing.Point(186, 131);
+            this.txtStudentName.Name = "txtStudentName";
+            this.txtStudentName.Size = new System.Drawing.Size(200, 26);
+            this.txtStudentName.TabIndex = 7;
+            // 
+            // dtpDayOfBirth
+            // 
+            this.dtpDayOfBirth.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDayOfBirth.Location = new System.Drawing.Point(186, 216);
+            this.dtpDayOfBirth.Name = "dtpDayOfBirth";
+            this.dtpDayOfBirth.Size = new System.Drawing.Size(200, 26);
+            this.dtpDayOfBirth.TabIndex = 8;
+            // 
+            // rdbMale
+            // 
+            this.rdbMale.AutoSize = true;
+            this.rdbMale.Location = new System.Drawing.Point(546, 54);
+            this.rdbMale.Name = "rdbMale";
+            this.rdbMale.Size = new System.Drawing.Size(65, 24);
+            this.rdbMale.TabIndex = 9;
+            this.rdbMale.TabStop = true;
+            this.rdbMale.Text = "Male";
+            this.rdbMale.UseVisualStyleBackColor = true;
+            // 
+            // rdbFemale
+            // 
+            this.rdbFemale.AutoSize = true;
+            this.rdbFemale.Location = new System.Drawing.Point(656, 54);
+            this.rdbFemale.Name = "rdbFemale";
+            this.rdbFemale.Size = new System.Drawing.Size(86, 24);
+            this.rdbFemale.TabIndex = 10;
+            this.rdbFemale.TabStop = true;
+            this.rdbFemale.Text = "Female";
+            this.rdbFemale.UseVisualStyleBackColor = true;
+            // 
+            // cbClass
+            // 
+            this.cbClass.FormattingEnabled = true;
+            this.cbClass.Location = new System.Drawing.Point(546, 128);
+            this.cbClass.Name = "cbClass";
+            this.cbClass.Size = new System.Drawing.Size(196, 28);
+            this.cbClass.TabIndex = 11;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(546, 170);
+            this.txtAddress.Multiline = true;
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(196, 72);
+            this.txtAddress.TabIndex = 12;
+            // 
+            // dgvStudentInfomation
+            // 
+            this.dgvStudentInfomation.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStudentInfomation.Location = new System.Drawing.Point(43, 450);
+            this.dgvStudentInfomation.Name = "dgvStudentInfomation";
+            this.dgvStudentInfomation.Size = new System.Drawing.Size(545, 274);
+            this.dgvStudentInfomation.TabIndex = 2;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Red;
+            this.btnExit.BackgroundImage = global::StudentManagement.Properties.Resources.Thoat;
+            this.btnExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.Black;
+            this.btnExit.Location = new System.Drawing.Point(775, 640);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(108, 84);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.UseVisualStyleBackColor = false;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnCancel.BackgroundImage = global::StudentManagement.Properties.Resources.Huy;
+            this.btnCancel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.ForeColor = System.Drawing.Color.Black;
+            this.btnCancel.Location = new System.Drawing.Point(775, 545);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(108, 84);
+            this.btnCancel.TabIndex = 7;
+            this.btnCancel.UseVisualStyleBackColor = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnSave.BackgroundImage = global::StudentManagement.Properties.Resources.Luu;
+            this.btnSave.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.Color.Black;
+            this.btnSave.Location = new System.Drawing.Point(775, 450);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(108, 84);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.UseVisualStyleBackColor = false;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.BackColor = System.Drawing.Color.Olive;
+            this.btnEdit.BackgroundImage = global::StudentManagement.Properties.Resources.sua;
+            this.btnEdit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEdit.ForeColor = System.Drawing.Color.Black;
+            this.btnEdit.Location = new System.Drawing.Point(628, 640);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(108, 84);
+            this.btnEdit.TabIndex = 5;
+            this.btnEdit.UseVisualStyleBackColor = false;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnDelete.BackgroundImage = global::StudentManagement.Properties.Resources.Xoa;
+            this.btnDelete.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.ForeColor = System.Drawing.Color.Black;
+            this.btnDelete.Location = new System.Drawing.Point(629, 545);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(108, 84);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.UseVisualStyleBackColor = false;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.Teal;
+            this.btnAdd.BackgroundImage = global::StudentManagement.Properties.Resources.Them;
+            this.btnAdd.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.ForeColor = System.Drawing.Color.Black;
+            this.btnAdd.Location = new System.Drawing.Point(628, 450);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(108, 84);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.UseVisualStyleBackColor = false;
+            // 
+            // frmStudentManage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(0)))), ((int)(((byte)(62)))));
+            this.ClientSize = new System.Drawing.Size(924, 766);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dgvStudentInfomation);
+            this.Controls.Add(this.gbStudentInfo);
+            this.Controls.Add(this.label1);
+            this.Name = "frmStudentManage";
+            this.Text = "Student Management";
+            this.gbStudentInfo.ResumeLayout(false);
+            this.gbStudentInfo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudentInfomation)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox gbStudentInfo;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Label lblClass;
+        private System.Windows.Forms.Label lblStudentGender;
+        private System.Windows.Forms.Label lblStudentDOB;
+        private System.Windows.Forms.Label lblStudentName;
+        private System.Windows.Forms.Label lblStudentID;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.ComboBox cbClass;
+        private System.Windows.Forms.RadioButton rdbFemale;
+        private System.Windows.Forms.RadioButton rdbMale;
+        private System.Windows.Forms.DateTimePicker dtpDayOfBirth;
+        private System.Windows.Forms.TextBox txtStudentName;
+        private System.Windows.Forms.TextBox txtStudentID;
+        private System.Windows.Forms.DataGridView dgvStudentInfomation;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ StudentManagement/StudentManagement/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentManagement
+{
+    public partial class frmStudentManage : Form
+    {
+        public frmStudentManage()
+        {
+            InitializeComponent();
+        }
+
+        private void gbStudentInfo_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblClass_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblStudentGender_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblStudentID_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblStudentName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblStudentDOB_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblAddress_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
